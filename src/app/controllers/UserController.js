b60a5949e76d6a667014f224d92ca6123e1ee2f6
alisson <alisson-henri@live.com>
2019-05-07 17:24:08 -0300

    Session

--- src/app/controllers/UserController.js
+++ src/app/controllers/UserController.js
@@ -1,17 +1,17 @@
 const { User } = require('../models')
 
 class UserController {
-  create(req, res) {
+  create (req, res) {
     return res.render('auth/signup')
   }
-  git
 
-  async store(req, res) {
+  async store (req, res) {
     const { filename: avatar } = req.file
 
-    User.create({ ...req.body, avatar: avatar })
-    User.create(req.body)
+    await User.create({ ...req.body, avatar })
+
     return res.redirect('/')
   }
 }
+
 module.exports = new UserController()
--- src/routes.js
+++ src/routes.js
@@ -2,9 +2,16 @@ const express = require('express')
 
 const routes = express.Router()
 const UserController = require('./app/controllers/UserController')
+const SessionController = require('./app/controllers/SessionController')
 const multerConfig = require('./config/multer')
 const upload = require('multer')(multerConfig)
 
+routes.get('/', SessionController.create)
+routes.post('/signin', SessionController.store)
 routes.get('/signup', UserController.create)
 routes.post('/signup', upload.single('avatar'), UserController.store)
+
+routes.post('/app/dashboard', (req, res) => {
+  res.render('dashboard')
+})
 module.exports = routes
